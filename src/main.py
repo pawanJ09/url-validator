1ca48a0e2d93aaad8109af233b823e913afc2c48
Pawan Jaiswal <pawan.jaiswal09@gmail.com>
2023-12-05 14:42:05 -0600

    Adding logic to fetch column names from excel file and identify columns with URL

--- src/main.py
+++ src/main.py
@@ -2,3 +2,19 @@ import pandas as pd
 
 if __name__ == "__main__":
     print('Starting with URL Validator')
+    df = pd.read_excel('../sheet/test-urls.xlsx', sheet_name='Sheet1')
+    print('Excel file contents: ')
+    print(df)
+    cols = df.keys().values
+    cols_list = list()
+    for col in cols:
+        if 'url' in col.lower():
+            cols_list.append(col)
+    if len(cols_list) > 0:
+        print(f'\nFetching data for columns: {cols_list}')
+        url_df = pd.DataFrame(df.loc[:, cols_list])
+        url_list = url_df.values.tolist()
+        print(f'\nURLs found: {url_list}')
+    else:
+        print('No columns containing URL found. Quiting program.')
+
